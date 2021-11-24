522cd34be24098360b5a6f9c8326308521d1bce2
MD: Suvo <santo35702@gmail.com>
2021-11-24 11:26:43 +0600

    Shop: Home page Collection & Brand logo set

--- resources/views/livewire/frontend/home-page.blade.php
+++ resources/views/livewire/frontend/home-page.blade.php
@@ -466,55 +466,55 @@
     <div class="collection-box section">
         <div class="container-fluid">
             <div class="collection-grid">
-                    <div class="collection-grid-item">
-                        <a href="collection-page.html" class="collection-grid-item__link">
-                            <img data-src="assets/images/collection/fashion.jpg" src="assets/images/collection/fashion.jpg" alt="Fashion" class="blur-up lazyload"/>
-                            <div class="collection-grid-item__title-wrapper">
-                                <h3 class="collection-grid-item__title btn btn--secondary no-border">Fashion</h3>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="collection-grid-item">
-                        <a href="collection-page.html" class="collection-grid-item__link">
-                            <img class="blur-up lazyload" data-src="assets/images/collection/cosmetic.jpg" src="assets/images/collection/cosmetic.jpg" alt="Cosmetic"/>
-                            <div class="collection-grid-item__title-wrapper">
-                                <h3 class="collection-grid-item__title btn btn--secondary no-border">Cosmetic</h3>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="collection-grid-item blur-up lazyloaded">
-                        <a href="collection-page.html" class="collection-grid-item__link">
-                            <img data-src="assets/images/collection/bag.jpg" src="assets/images/collection/bag.jpg" alt="Bag" class="blur-up lazyload"/>
-                            <div class="collection-grid-item__title-wrapper">
-                                <h3 class="collection-grid-item__title btn btn--secondary no-border">Bag</h3>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="collection-grid-item">
-                        <a href="collection-page.html" class="collection-grid-item__link">
-                            <img data-src="assets/images/collection/accessories.jpg" src="assets/images/collection/accessories.jpg" alt="Accessories" class="blur-up lazyload"/>
-                            <div class="collection-grid-item__title-wrapper">
-                                <h3 class="collection-grid-item__title btn btn--secondary no-border">Accessories</h3>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="collection-grid-item">
-                        <a href="collection-page.html" class="collection-grid-item__link">
-                            <img data-src="assets/images/collection/shoes.jpg" src="assets/images/collection/shoes.jpg" alt="Shoes" class="blur-up lazyload"/>
-                            <div class="collection-grid-item__title-wrapper">
-                                <h3 class="collection-grid-item__title btn btn--secondary no-border">Shoes</h3>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="collection-grid-item">
-                        <a href="collection-page.html" class="collection-grid-item__link">
-                            <img data-src="assets/images/collection/jewellry.jpg" src="assets/images/collection/jewellry.jpg" alt="Jewellry" class="blur-up lazyload"/>
-                            <div class="collection-grid-item__title-wrapper">
-                                <h3 class="collection-grid-item__title btn btn--secondary no-border">Jewellry</h3>
-                            </div>
-                        </a>
-                    </div>
+                <div class="collection-grid-item">
+                    <a href="collection-page.html" class="collection-grid-item__link">
+                        <img data-src="{{ asset('assets/images/collection/modern-fi-1.jpg') }}" src="{{ asset('assets/images/collection/modern-fi-1.jpg') }}" alt="Fashion" class="blur-up lazyload"/>
+                        <div class="collection-grid-item__title-wrapper">
+                            <h3 class="collection-grid-item__title btn btn--secondary no-border">Fashion</h3>
+                        </div>
+                    </a>
                 </div>
+                <div class="collection-grid-item">
+                    <a href="collection-page.html" class="collection-grid-item__link">
+                        <img class="blur-up lazyload" data-src="{{ asset('assets/images/collection/cosmetic.jpg') }}" src="{{ asset('assets/images/collection/cosmetic.jpg') }}" alt="Cosmetic"/>
+                        <div class="collection-grid-item__title-wrapper">
+                            <h3 class="collection-grid-item__title btn btn--secondary no-border">Cosmetic</h3>
+                        </div>
+                    </a>
+                </div>
+                <div class="collection-grid-item blur-up lazyloaded">
+                    <a href="collection-page.html" class="collection-grid-item__link">
+                        <img data-src="{{ asset('assets/images/collection/bag.jpg') }}" src="{{ asset('assets/images/collection/bag.jpg') }}" alt="Bag" class="blur-up lazyload"/>
+                        <div class="collection-grid-item__title-wrapper">
+                            <h3 class="collection-grid-item__title btn btn--secondary no-border">Bag</h3>
+                        </div>
+                    </a>
+                </div>
+                <div class="collection-grid-item">
+                    <a href="collection-page.html" class="collection-grid-item__link">
+                        <img data-src="{{ asset('assets/images/collection/accessories.jpg') }}" src="{{ asset('assets/images/collection/accessories.jpg') }}" alt="Accessories" class="blur-up lazyload"/>
+                        <div class="collection-grid-item__title-wrapper">
+                            <h3 class="collection-grid-item__title btn btn--secondary no-border">Accessories</h3>
+                        </div>
+                    </a>
+                </div>
+                <div class="collection-grid-item">
+                    <a href="collection-page.html" class="collection-grid-item__link">
+                        <img data-src="{{ asset('assets/images/collection/shoes.jpg') }}" src="{{ asset('assets/images/collection/shoes.jpg') }}" alt="Shoes" class="blur-up lazyload"/>
+                        <div class="collection-grid-item__title-wrapper">
+                            <h3 class="collection-grid-item__title btn btn--secondary no-border">Shoes</h3>
+                        </div>
+                    </a>
+                </div>
+                <div class="collection-grid-item">
+                    <a href="collection-page.html" class="collection-grid-item__link">
+                        <img data-src="{{ asset('assets/images/collection/jewellry.jpg') }}" src="{{ asset('assets/images/collection/jewellry.jpg') }}" alt="Jewellry" class="blur-up lazyload"/>
+                        <div class="collection-grid-item__title-wrapper">
+                            <h3 class="collection-grid-item__title btn btn--secondary no-border">Jewellry</h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
     <!--End Collection Box slider-->
@@ -525,25 +525,25 @@
             <div class="row">
                 <div class="col-12 col-sm-12 col-md-12 col-lg-12">
                     <div class="logo-bar">
-                <div class="logo-bar__item">
-                    <img src="assets/images/logo/brandlogo1.png" alt="" title="" />
-                </div>
-                <div class="logo-bar__item">
-                    <img src="assets/images/logo/brandlogo2.png" alt="" title="" />
-                </div>
-                <div class="logo-bar__item">
-                    <img src="assets/images/logo/brandlogo3.png" alt="" title="" />
-                </div>
-                <div class="logo-bar__item">
-                    <img src="assets/images/logo/brandlogo4.png" alt="" title="" />
-                </div>
-                <div class="logo-bar__item">
-                    <img src="assets/images/logo/brandlogo5.png" alt="" title="" />
-                </div>
-                <div class="logo-bar__item">
-                    <img src="assets/images/logo/brandlogo6.png" alt="" title="" />
-                </div>
-            </div>
+                        <div class="logo-bar__item">
+                            <img src="{{ asset('assets/images/logo/brandlogo1.png') }}" alt="" title="" />
+                        </div>
+                        <div class="logo-bar__item">
+                            <img src="{{ asset('assets/images/logo/brandlogo2.png') }}" alt="" title="" />
+                        </div>
+                        <div class="logo-bar__item">
+                            <img src="{{ asset('assets/images/logo/brandlogo3.png') }}" alt="" title="" />
+                        </div>
+                        <div class="logo-bar__item">
+                            <img src="{{ asset('assets/images/logo/brandlogo4.png') }}" alt="" title="" />
+                        </div>
+                        <div class="logo-bar__item">
+                            <img src="{{ asset('assets/images/logo/brandlogo5.png') }}" alt="" title="" />
+                        </div>
+                        <div class="logo-bar__item">
+                            <img src="{{ asset('assets/images/logo/brandlogo6.png') }}" alt="" title="" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
