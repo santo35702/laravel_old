05a890db34968edfe883936215d1e124622c2492
MD: Suvo <santo35702@gmail.com>
2021-11-25 16:00:51 +0600

    Database: seeder must be commented after seed db, else it will re-genarate same data in SQL server

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,9 +18,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        User::factory(5)->create();
-        Brand::factory(10)->create();
-        Category::factory(10)->create();
-        Product::factory(50)->create();
+        // User::factory(5)->create();
+        // Brand::factory(10)->create();
+        // Category::factory(10)->create();
+        // Product::factory(50)->create();
     }
 }
