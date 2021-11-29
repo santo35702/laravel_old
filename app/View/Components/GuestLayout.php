2a9dd642c14af70074bf29b2a6716a02093aa974
MD: Suvo <santo35702@gmail.com>
2021-11-29 17:18:11 +0600

    Shop: Authentication Template change

--- app/View/Components/GuestLayout.php
+++ app/View/Components/GuestLayout.php
@@ -14,6 +14,6 @@ class GuestLayout extends Component
     public function render()
     {
         // return view('layouts.guest');
-        return view('layouts.admin');
+        return view('layouts.auth');
     }
 }
