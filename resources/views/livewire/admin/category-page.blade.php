00308af1befd330b4c2d44417a2cd94ad1ad05c1
MD: Suvo <santo35702@gmail.com>
2021-12-03 17:40:27 +0600

    Admin: Category page done

---
+++ resources/views/livewire/admin/category-page.blade.php
@@ -0,0 +1,74 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Categories</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Home</a></li>
+                        <li class="breadcrumb-item active">Categories</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card card-success card-outline shadow">
+                        <div class="card-header">
+                            <h3 class="card-title">All Categories List</h3>
+                            <div class="card-tools">
+                                <a href="{{ route('admin.categories.add') }}" class="btn btn-sm btn-primary">Add new</a>
+                                <button type="button" class="btn btn-tool" data-card-widget="maximize" title="Expand">
+                                    <i class="fas fa-expand"></i>
+                                </button>
+                            </div>
+                            <!-- /.card-tools -->
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table id="example1" class="table table-bordered table-hover table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>SL</th>
+                                        <th>Name</th>
+                                        <th>Slug</th>
+                                        <th>Description</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php
+                                        $sl = 1;
+                                    @endphp
+                                    @foreach ($categories as $key)
+                                        <tr>
+                                            <td>{{ $sl++ }}</td>
+                                            <td>{{ $key->name }}</td>
+                                            <td>{{ $key->slug }}</td>
+                                            <td>{{ $key->description }}</td>
+                                            <td>-</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
