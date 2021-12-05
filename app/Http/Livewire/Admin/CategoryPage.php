783f557e7d2bc073e6faaaf1524de74975855632
MD: Suvo <santo35702@gmail.com>
2021-12-05 13:26:25 +0600

    Admin: Category Delete Success

--- app/Http/Livewire/Admin/CategoryPage.php
+++ app/Http/Livewire/Admin/CategoryPage.php
@@ -4,9 +4,18 @@ namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
 use App\Models\Category;
+use Illuminate\Http\Request;
 
 class CategoryPage extends Component
 {
+    public function deleteItem(Request $request, $id)
+    {
+        $category = Category::find($id);
+        $category->delete();
+        $request->session()->flash('status', 'Category has been Deleted successfully!');
+        return redirect()->route('admin.categories.index');
+    }
+
     public function render()
     {
         $categories = Category::get();
