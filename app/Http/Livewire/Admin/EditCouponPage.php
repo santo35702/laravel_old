2e2d5392ad943701ab9485b32fe28d85c561b939
MD: Suvo <santo35702@gmail.com>
2021-12-27 12:00:35 +0600

    Admin: Coupon Edit Done by own

--- app/Http/Livewire/Admin/EditCouponPage.php
+++ app/Http/Livewire/Admin/EditCouponPage.php
@@ -3,9 +3,54 @@
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
+use App\Models\Coupon;
+use Illuminate\Http\Request;
 
 class EditCouponPage extends Component
 {
+    public $code;
+    public $type;
+    public $value;
+    public $cart_value;
+
+    public function mount($id)
+    {
+        $coupon = Coupon::find($id);
+        $this->code = $coupon->code;
+        $this->type = $coupon->type;
+        $this->value = $coupon->value;
+        $this->cart_value = $coupon->cart_value;
+        $this->coupon_id = $coupon->id;
+    }
+
+    public function updated($fields)
+    {
+        $this->validateOnly($fields, [
+            'code' => 'required|unique:coupons',
+            'type' => 'required',
+            'value' => 'required|numeric',
+            'cart_value' => 'required|numeric',
+        ]);
+    }
+
+    public function updateItem(Request $request)
+    {
+        $this->validate([
+            'code' => 'required',
+            'type' => 'required',
+            'value' => 'required|numeric',
+            'cart_value' => 'required|numeric',
+        ]);
+
+        $coupon = Coupon::find($this->coupon_id);
+        $coupon->code = $this->code;
+        $coupon->type = $this->type;
+        $coupon->value = $this->value;
+        $coupon->cart_value = $this->cart_value;
+        $coupon->save();
+        $request->session()->flash('status', 'Coupon has been updated successfully!');
+    }
+
     public function render()
     {
         return view('livewire.admin.edit-coupon-page')->layout('layouts.admin');
--- resources/views/livewire/admin/edit-coupon-page.blade.php
+++ resources/views/livewire/admin/edit-coupon-page.blade.php
@@ -1,3 +1,105 @@
-<div>
-    {{-- The whole world belongs to you. --}}
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Edit Coupons</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Home</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('admin.coupons.index') }}">Coupons</a></li>
+                        <li class="breadcrumb-item active">Edit [{{ $code }}]</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">Coupons Information</h3>
+
+                            <div class="card-tools">
+                                <a href="{{ route('admin.coupons.index') }}" class="btn btn-tool"><i class="fas fa-undo"></i></a>
+                            </div>
+                        </div>
+                        <!-- /.card-header -->
+                        <form wire:submit.prevent="updateItem">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-sm-8 mx-auto">
+                                        @if (session('status'))
+                                            <div class="alert alert-success text-uppercase alert-dismissible fade show" role="alert">
+                                                {{ session('status') }}
+                                                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                            </div>
+                                        @endif
+
+                                        <div class="form-group">
+                                            <label for="code">Coupon Code <span class="text-danger">*</span></label>
+                                            <input type="text" class="form-control form-control-border @error('code') is-invalid @enderror" id="code" placeholder="Coupon Code" wire:model="code">
+                                            @error ('code')
+                                                <div class="invalid-feedback">
+                                                    {{ $message }}
+                                                </div>
+                                            @enderror
+                                        </div><!-- /.form-group -->
+
+                                        <div class="form-group">
+                                            <label for="type">Coupon Type <span class="text-danger">*</span></label>
+                                            <select class="custom-select form-control-border @error('type') is-invalid @enderror" id="type" wire:model="type">
+                                                <option>Select</option>
+                                                <option value="fixed">Fixed</option>
+                                                <option value="percent">Percent</option>
+                                            </select>
+                                            @error ('type')
+                                                <div class="invalid-feedback">
+                                                    {{ $message }}
+                                                </div>
+                                            @enderror
+                                        </div><!-- /.form-group -->
+
+                                        <div class="form-group">
+                                            <label for="value">Coupon Value <span class="text-danger">*</span></label>
+                                            <input type="text" class="form-control form-control-border @error('value') is-invalid @enderror" id="value" placeholder="Coupon Code" wire:model="value">
+                                            @error ('value')
+                                                <div class="invalid-feedback">
+                                                    {{ $message }}
+                                                </div>
+                                            @enderror
+                                        </div><!-- /.form-group -->
+
+                                        <div class="form-group">
+                                            <label for="cart_value">Cart Value <span class="text-danger">*</span></label>
+                                            <input type="text" class="form-control form-control-border @error('cart_value') is-invalid @enderror" id="cart_value" placeholder="Coupon Code" wire:model="cart_value">
+                                            @error ('cart_value')
+                                                <div class="invalid-feedback">
+                                                    {{ $message }}
+                                                </div>
+                                            @enderror
+                                        </div><!-- /.form-group -->
+                                    </div><!-- /.col-sm-8 .mx-auto -->
+                                </div><!-- /.row -->
+                            </div><!-- /.card-body -->
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-sm-8 mx-auto">
+                                        <button type="submit" class="btn btn-primary float-right">Update</button>
+                                    </div><!-- /.col-sm-8 .mx-auto -->
+                                </div><!-- /.row -->
+                            </div><!-- /.card-footer -->
+                        </form>
+                    </div><!-- /.card -->
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
 </div>
